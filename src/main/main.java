fa616baa643d84c93a3fd8b6254e63e0b38619ca
TomasB22 <t.baeza01@ufromail.cl>
2021-12-01 08:52:21 -0300

    .

--- src/main/main.java
+++ src/main/main.java
@@ -3,5 +3,6 @@ package main;
 public class main {
     public static void main(String[] args) {
         System.out.println ("Primer commit.");
+        System.out.println ("Commit de thanya");
     }
 }
